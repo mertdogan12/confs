c4e964065920871c614c31ce17504bd8229fe113
Mert Dogan <mert.dogan@me.com>
2021-12-18 23:45:25 +0100

    tmux.sh update
    Now just creates the session "cmd" at the start (or attachtes to it, if
    already exists).

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,9 +1,1 @@
-# Starts programming session if not already exist
-if [ $(tmux has-session -t prg 2>/dev/null; echo $?) = 1 ]; then
-    export PROJECTPATH=$1
-    tmux new-session -d -s prg -n code 'cd $PROJECTPATH; nvim .; zsh'
-    tmux new-window -t prg -n lazygit 'cd $PROJECTPATH; lazygit; zsh'
-    tmux new-window -t prg 'cd $PROJECTPATH; zsh'
-fi
-
-tmux new-session -A -s prg
+tmux new -A -s main -n code

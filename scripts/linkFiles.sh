c7ea343c7602d253c127d5b03fd780c827ec717c
Mert Dogan <mert.dogan@me.com>
2022-03-16 15:37:25 +0100

    Folder restucture
    also linkFiels.sh changed

--- scripts/linkFiles.sh
+++ scripts/linkFiles.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-find $(git ls-files)            \
-    -type f                     \
-    -not -path "*git*"          \
-    -a -not -name \*.sh         \
-    -a -not -name \*.md         \
-    -a -not -path "_config.yml" \
+find $(git ls-files)                \
+    -type f                         \
+    -not -path "*git*"              \
+    -a -path ".gitconfig"           \
+    -a -not -name \*.sh             \
+    -a -not -name \*.md             \
+    -a -not -path "_config.yml"     \
+    -a -not -path "selected"        \
+    -a -not -path "screenshot.png"
 

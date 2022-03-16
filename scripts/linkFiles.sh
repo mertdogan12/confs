500198c55783f3ff1c723cdc5e5f83167f64249e
Mert Dogan <mert.dogan@me.com>
2022-03-16 18:02:07 +0100

    linkeFiles works

--- scripts/linkFiles.sh
+++ scripts/linkFiles.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-find $(git ls-files)                \
-    -type f                         \
-    -not -path "*git*"              \
-    -a -path ".gitconfig"           \
-    -a -not -name \*.sh             \
-    -a -not -name \*.md             \
-    -a -not -path "_config.yml"     \
-    -a -not -path "selected"        \
-    -a -not -path "screenshot.png"
-
+for i in $(find $(git ls-files)              \
+    -type f                                 \
+    -not -path ".gitignore"                 \
+    -a -not -name \*.sh                     \
+    -a -not -name \*.md                     \
+    -a -not -path "_config.yml"             \
+    -a -not -path "selected"                \
+    -a -not -path "screenshot.png"); do	
+	rm -v ~/$i
+	mkdir -p ~/$(dirname $i)
+	ln -s ~/.dotfiles/$i ~/$i
+done
